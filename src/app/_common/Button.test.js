bfddb4c746186ac73a8e3590f1f2ce620d79a628
yutounun <geek.yuto@gmail.com>
2023-10-25 08:26:59 +0900

    update button UT

--- src/app/_common/Button.test.js
+++ src/app/_common/Button.test.js
@@ -5,22 +5,28 @@ import Button from "./Button";
 describe("Button", () => {
   it("renders continue button correctly", () => {
     render(<Button continue />);
-    expect(screen.getByText("Continue")).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: /continue/i })
+    ).toBeInTheDocument();
   });
   it("renders modal button correctly", () => {
     render(<Button modal text="save"></Button>);
-    expect(screen.getByText(/save/i)).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: /save/i })).toBeInTheDocument();
   });
   it("renders cancel button correctly", () => {
     render(<Button cancel />);
-    expect(screen.getByText(/cancel/i)).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: /cancel/i })).toBeInTheDocument();
   });
   it("renders new button correctly", () => {
     render(<Button new />);
-    expect(screen.getByText(/add new/i)).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: /add new/i })
+    ).toBeInTheDocument();
   });
   it("renders normal button correctly", () => {
     render(<Button normal text="Click me"></Button>);
-    expect(screen.getByText(/Click me/i)).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: /Click me/i })
+    ).toBeInTheDocument();
   });
 });
