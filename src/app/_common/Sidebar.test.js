09eae99d164e359e76f64e5cc4fe1ac3edc3bc96
yutounun <geek.yuto@gmail.com>
2023-10-26 08:25:23 +0900

    implement test for sidebar

---
+++ src/app/_common/Sidebar.test.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Sidebar from "./Sidebar";
+
+function runSidebarTest(title, type) {
+  test(title, async () => {
+    render(<Sidebar active={type} />);
+    const taskLink = screen.getByRole("link", {
+      name: /tasks/i,
+    });
+    const projectLink = screen.getByRole("link", {
+      name: /projects/i,
+    });
+
+    if (type === "tasks") {
+      expect(taskLink).toHaveClass("text-white");
+      expect(projectLink).not.toHaveClass("text-white");
+    }
+
+    if (type === "projects") {
+      expect(taskLink).not.toHaveClass("text-white");
+      expect(projectLink).toHaveClass("text-white");
+    }
+  });
+}
+
+describe("Tabs", () => {
+  runSidebarTest("click tabs and check the focused tab", "tasks");
+
+  runSidebarTest("find Projects link active on projects page", "projects");
+});
