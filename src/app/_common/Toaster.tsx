cfd8dfea3365f5fdb467d7797ba39d41d69dd06a
yutounun <geek.yuto@gmail.com>
2023-11-11 18:36:28 +0900

    fix toast style

--- src/app/_common/Toaster.tsx
+++ src/app/_common/Toaster.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Toaster = ({ message }) => {
   return (
-    <div className="toast toast-top toast-center">
-      <div className="alert alert-info">
+    <div className="z-10 fixed top-0 rounded-lg bg-green-600 px-10 py-5  left-1/2 transform -translate-x-1/2 font-semibold">
+      <div className="text-white">
         <span>{message}</span>
       </div>
     </div>
